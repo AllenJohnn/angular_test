a83f9ea06831b72770a3db59fd5b041c4e2b06eb
AllenJohnn <allenjohnjoy2004@gmail.com>
2026-07-23 12:16:50 +0530

    day4 noon

--- project/campus360/js/app.js
+++ project/campus360/js/app.js
@@ -1,29 +1,37 @@
 var app = angular.module("campus360", []);
 
 app.controller("dashboardController", function ($scope) {
-  $scope.collegename = "FISAT";
+  // Imported updated initial values from 2nd JS
+  $scope.collegename = "FiSaT";
   $scope.collegeId = "FISAT-2026";
   $scope.totalstudents = 1200;
-  $scope.totalfaculty = 85;
-
+  $scope.totalfaculty = 120; // Updated from 85 to 120
   $scope.students = ["Shahid", "John", "Allen", "Shaun"];
   $scope.showstudents = true;
-
-  $scope.fees = 2500;
+  $scope.fees = 25600; // Updated from 2500 to 25600
   $scope.today = new Date();
 
-  $scope.departments = ["CSE", "MCA", "MBA", "EEE", "ECE"];
-  $scope.selectedDepartment = "CSE";
+  $scope.departments = ["CSE", "MCA", "MBA", "EEE", "ECE", "BCA", "IMCA"];
+  $scope.selectedDepartment = "MCA"; // Default updated to MCA
 
-  // Actions
   $scope.addStudent = function () {
     $scope.totalstudents++;
   };
 
   $scope.isDisable = true;
-  $scope.isReadOnly = true;
-
   $scope.allowAdmission = function () {
-    $scope.isDisable = !$scope.isDisable;
+    $scope.isDisable = false; // Directly enables admission (from 2nd JS logic)
+  };
+
+  $scope.isReadOnly = true;
+  $scope.toggleReadOnly = function () {
+    $scope.isReadOnly = !$scope.isReadOnly;
+  };
+
+  $scope.student = {};
+
+  $scope.registerStudent = function () {
+
+    alert("student registered successfully!");
   };
 });
