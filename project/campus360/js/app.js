e9ab5214b66886516e652ead966772f70538c4ba
AllenJohnn <allenjohnjoy2004@gmail.com>
2026-07-23 09:21:56 +0530

    day 4 mrng

--- project/campus360/js/app.js
+++ project/campus360/js/app.js
@@ -2,21 +2,28 @@ var app = angular.module("campus360", []);
 
 app.controller("dashboardController", function ($scope) {
   $scope.collegename = "FISAT";
+  $scope.collegeId = "FISAT-2026";
   $scope.totalstudents = 1200;
   $scope.totalfaculty = 85;
 
   $scope.students = ["Shahid", "John", "Allen", "Shaun"];
+  $scope.showstudents = true;
 
   $scope.fees = 2500;
   $scope.today = new Date();
 
-  $scope.departments = ["CSE", "MCA", "MBA"];
-
+  $scope.departments = ["CSE", "MCA", "MBA", "EEE", "ECE"];
   $scope.selectedDepartment = "CSE";
 
-  $scope.showstudents = true;
-
+  // Actions
   $scope.addStudent = function () {
     $scope.totalstudents++;
+  };
+
+  $scope.isDisable = true;
+  $scope.isReadOnly = true;
+
+  $scope.allowAdmission = function () {
+    $scope.isDisable = !$scope.isDisable;
   };
 });
